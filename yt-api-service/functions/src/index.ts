de5ac1e8b5556d5772c50974bbda249544a8a090
Denzy <izahchidimma@gmail.com>
2025-05-03 18:32:13 -0500

    updated dependencies and generateUploadUrl to v2

--- yt-api-service/functions/src/index.ts
+++ yt-api-service/functions/src/index.ts
@@ -2,6 +2,11 @@ import {Firestore} from "firebase-admin/firestore";
 import * as logger from "firebase-functions/logger";
 import {auth} from "firebase-functions/v1";
 import * as admin from "firebase-admin";
+import { onCall , HttpsError} from "firebase-functions/v2/https";
+import {Storage} from "@google-cloud/storage";
+
+const storage = new Storage();
+const rawVideoBucketName = "growingwd-yt-raw-videos";
 
 
 admin.initializeApp();
@@ -20,5 +25,33 @@ export const createUser = auth.user().onCreate(async (user) => {
   firestore.collection("users").doc(user.uid).set(userInfo);
   logger.info(`User Created: ${JSON.stringify(userInfo)}`);
   return;
+
 });
+
+export const generateUploadUrl = onCall({maxInstances: 1}, async (request) => {
+  // Check if the user is authentication (Updated for firebase v2)
+  if (!request.auth) {
+    throw new HttpsError(
+      "failed-precondition",
+      "The function must be called while authenticated."
+    );
+  }
+
+  const auth = request.auth;
+  const data = request.data;
+  const bucket = storage.bucket(rawVideoBucketName);
+
+  // Generate a unique filename for upload
+  const fileName = `${auth.uid}-${Date.now()}.${data.fileExtension}`;
+
+  // Get a v4 signed URL for uploading file
+  const [url] = await bucket.file(fileName).getSignedUrl({
+    version: "v4",
+    action: "write",
+    expires: Date.now() + 15 * 60 * 1000, // 15 minutes
+  });
+
+  return {url, fileName};
+});
+
 
